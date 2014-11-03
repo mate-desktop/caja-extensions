1ffcee39f517957a742da5290153c6106851aada
Stefano Karapetsas <stefano@karapetsas.com>
2014-11-03 15:06:37 +0100

    Add information for about dialogs

--- open-terminal/open-terminal.c
+++ open-terminal/open-terminal.c
@@ -17,7 +17,7 @@
  *  License along with this library; if not, write to the Free
  *  Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
  *
- *  Author: Christian Neumair <chris@mate-de.org>
+ *  Author: Christian Neumair <chris@gnome-de.org>
  * 
  */
 
